849f902df8cb65f64fa5e2e187d49240dfe24d93
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-05-28 03:07:15 +0300

    Create ResultActivity.java

---
+++ app/src/main/java/com/example/firsapp/ResultActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class ResultActivity extends Activity {
+
+//    boolean[] results=new boolean[6];
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_result);
+       
+    Intent intent=getIntent();
+    String results= intent.getStringExtra("results");
+
+    }
+}
